f488b6f627fa6fda14708c9ae68164cecf384770
Boen <1997@boenfu.cn>
2019-02-17 18:56:13 +0800

    Init with spring-boot

---
+++ src/main/java/cn/uicab/uicab/UicabApplication.java
@@ -0,0 +1,13 @@
+package cn.uicab.uicab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UicabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UicabApplication.class, args);
+    }
+
+}
